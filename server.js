5548be32334d76b855e39403a8cde0b8d91b32ee
neelchhatbar <neelchhatbar@gmail.com>
2022-12-13 18:29:28 +0530

    edited server

--- server.js
+++ server.js
@@ -2,24 +2,46 @@ const Dotenv = require('dotenv');
 const Express = require('express');
 const App = Express();
 const Cors = require('cors');
+const mongoose = require('mongoose');
 const path = require("path");
-Dotenv.config({path: './config.env'})
+Dotenv.config({ path: './config.env' })
 App.use(Cors());
 
 App.use(Express.json())
+
+const DB = process.env.DATABASE;
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected `);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 
 App.use(require("./router/Auth"))
 
 const PORT = process.env.PORT || 5000;
 
-// App.use(Express.static(path.join(__dirname, "./client/build")));
+connectDB().then(() => {
+    App.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 
-// App.get("*", (req, res) => {
+    App.use(Express.static(path.join(__dirname, "./client/build")));
 
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"), function(err){res.status(500).send(err);});
+    App.get("*", (req, res) => {
 
-// })
+        res.sendFile(path.join(__dirname, "./client/build/index.html"), function (err) { res.status(500).send(err); });
 
-App.listen(PORT);
+    })
+})
+
+// App.listen(PORT);
 
 // "client": "npm start --prefix client",
